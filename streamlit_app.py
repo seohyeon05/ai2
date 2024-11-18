dc48bb689d95876c538a9160bcf44b00ceaeede1
seohyeon05 <20242815@sdhs.gwe.hs.kr>
2024-11-18 10:17:33 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,12 +51,12 @@ if uploaded_file is not None:
     # 클래스별 확률을 HTML과 CSS로 시각화
     st.markdown("<h3>클래스별 확률:</h3>", unsafe_allow_html=True)
 
-     if prediction == labels[0]:
-         st.write("중냉")
-     elif prediction == labels[1]:
-         st.write("짜장면 먹고싶다")
-     elif prediction == labels[2]:
-         st.write("짬뽕 맛있겠다")
+    if prediction == labels[0]:
+        st.write("중냉")
+    elif prediction == labels[1]:
+        st.write("짜장면 먹고싶다")
+    elif prediction == labels[2]:
+        st.write("짬뽕 맛있겠다")
 
     for label, prob in zip(labels, probs):
         # HTML 및 CSS로 확률을 시각화
